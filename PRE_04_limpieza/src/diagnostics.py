fb4e2269906a0dcdd857d3610170f2555d9cb9a7
rutrujillog <rutrujillog@unal.edu.co>
2026-09-25 15:06:53 -0500

    Taller 4 resuelto

---
+++ PRE_04_limpieza/src/diagnostics.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+OUTPUT_FILE = "PRE_04_limpieza/submission/ventas.csv"
+
+
+def main():
+    df = pd.read_csv(OUTPUT_FILE)
+
+    series = df["amount"]
+
+    # series = series.astype(str)
+    # series = series[series.str.startswith("$")]
+
+    series = series.sort_values()
+    series = series.drop_duplicates()
+
+    print(series)
+    print(len(series))
+
+
+if __name__ == "__main__":
+    main()
+
+
